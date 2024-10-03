a07b25e8604b250cc6187cf41937f9d2a711fa74
Jakub Łuczak <jakub.luczak@summ-it.pl>
2024-10-03 20:42:55 +0200

    changed second arguments for admin controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController as AdminDashboardController;
+use App\Http\Controllers\Admin\StatsController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\TaskController;
@@ -77,11 +79,11 @@ Route::middleware('auth')->group(function() {
 
         // Task 10: point URL /admin/dashboard to a "Single Action" Admin/DashboardController
         // Put one code line here below
-        Route::get('dashboard', ['Admin\DashboardController']);
+        Route::get('dashboard', AdminDashboardController::class);
 
         // Task 11: point URL /admin/stats to a "Single Action" Admin/StatsController
         // Put one code line here below
-        Route::get('stats', ['Admin\StatsController']);
+        Route::get('stats', StatsController::class);
 
     // End of the /admin Route Group
     });
